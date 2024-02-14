41d993c1ed54b0feb50024bd392943129c505e07
denis.gubarkov <den.gubarkov@gmail.com>
2024-02-14 23:39:42 +0500

    1402242

--- dz140224.py
+++ dz140224.py
@@ -1,1 +1,2 @@
 print(list(filter(lambda x:x==x[::-1],('madam','fire','tomato','book','kiosk','mom'))))
+print(list(filter(lambda x:x==x[::-1],(input("Введите слово: ") for i in range(int(input("Введите количество слов: ")))))))
